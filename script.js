fd66d3dc82b42c1620bde859f921087519a8c3f3
Seksenova Begaiym <begaym01@gmail.com>
2023-02-21 14:53:56 +0600

    example4,example5

--- script.js
+++ script.js
@@ -3,8 +3,17 @@ console.log (exampel);
 
 
 let example2 = document.querySelector("p");
+example2.textContent= "Hello world";
+
 console.log(example2);
 
 
-let example3 = document.querySelector ("example3")
+let example3 = document.querySelector (".example3")
 console.log (example3);
+
+let example4 = document.querySelector ("#example4")
+console.log (example4);
+
+
+let example5 = document.querySelector ("#p:nth-child(4)")
+console.log (example5);
