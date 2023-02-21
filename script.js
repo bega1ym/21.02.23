7f149d522c28c77c91d27499992982583fb91623
Seksenova Begaiym <begaym01@gmail.com>
2023-02-21 15:40:05 +0600

    example7

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ console.log(exampel);
 
 let example2 = document.querySelector("p");
 example2.textContent = "Hello world";
-example2.style.color = "tomato"
-example2.style.fontSize = "12px"
+example2.style.color = "tomato";
+example2.style.fontSize = "12px";
 console.log(example2);
 
 let example3 = document.querySelector(".example3");
 example3.textContent = "Kyrgyzstan";
-example3.style.backgroundColor = "blue"
-example3.style.fontSize = "22px"
+example3.style.backgroundColor = "blue";
+example3.style.fontSize = "22px";
 console.log(example3);
 
 let example4 = document.querySelector("#example4");
@@ -22,10 +22,14 @@ let example5 = document.querySelector("p:nth-child(4)");
 console.log(example5);
 example5.textContent = "IT-Akadem";
 
-
-
-let example6 = document.querySelector ("article p");
-for(let i = 0; i< example6.clientHeight; i++){
+let example6 = document.querySelectorAll("article p");
+for (let i = 0; i < example6.length; i++) {
   let paragraph = example6[i];
-  paragraph.textContent +="❤";
+  paragraph.textContent += "👩‍💻🍕🍟";
 }
+
+let example7 = document.createElement ("div");
+console.log(example7.textContent = " Lorem ipsum 🐼")
+document.body.append(example7);
+example7.style.fontSize = "22px";
+example7.style.backgroundColor = "gray";
